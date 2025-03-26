f3b53513d2dfc154eaa3696704b551bb59e582c2
UltiRed8 <84141770+UltiRed8@users.noreply.github.com>
2025-03-26 11:18:30 +0100

    Added start of drag & drop

--- Source/UI/SceneWidget.cpp
+++ Source/UI/SceneWidget.cpp
@@ -2,9 +2,20 @@
 
 SceneWidget::SceneWidget(const bool& _openedByDefault) : Widget("Scene", _openedByDefault)
 {
+	onDroppedElement = Event<void, string>();
 }
 
 void SceneWidget::Draw()
 {
+	Image((ImTextureID)0, GetContentRegionAvail(), ImVec2(0.0f, 1.0f), ImVec2(1.0f, 0.0f)); // TODO draw scene texture
+	if (BeginDragDropTarget())
+	{
+		if (const ImGuiPayload* _payload = AcceptDragDropPayload("SPAWN_ACTOR"))
+		{
+			const string* _text = static_cast<string*>(_payload->Data);
+			onDroppedElement.Invoke(*_text);
+		}
+		EndDragDropTarget();
+	}
 }
 
--- Source/UI/SceneWidget.h
+++ Source/UI/SceneWidget.h
@@ -2,10 +2,15 @@
 
 #include "../Utils/CoreMinimal.h"
 #include "Widget.h"
+#include "Event.h"
 
 class SceneWidget : public Widget
 {
-	
+	Event<void, string> onDroppedElement;
+
+public:
+	FORCEINLINE Event<void, string>& OnDroppedElement() { return onDroppedElement; }
+
 public:
 	SceneWidget(const bool& _openedByDefault);
 	virtual ~SceneWidget() = default;
